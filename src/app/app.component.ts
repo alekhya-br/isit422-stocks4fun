fafc5b71bdee4f74037e0ba22cec3eb3d7fe56e1
Daryk <daryk.kohler@gmail.com>
2018-10-21 19:49:09 -0700

    Built dropdown dummy navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'isit422-stocks4fun';
 }
+
+(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+    $('#nav-toggle').click(function() {
+      $('nav ul').toggle();
+    });
+  });
+})(jQuery);
