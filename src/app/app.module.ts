1300504dc9978a62e4408b3fa7eb8f65f066322e
Daryk <daryk.kohler@gmail.com>
2018-10-21 20:33:40 -0700

    Added foundation for routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+]
 
 @NgModule({
   declarations: [
@@ -10,6 +19,10 @@ import { HomepageComponent } from './homepage/homepage.component';
     HomepageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } //Debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
