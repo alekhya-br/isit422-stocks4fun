576aeffe37b41d9a41b097acf737f64d3e4044d6
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2018-10-28 15:49:10 -0700

    split to client and server and added CRUD to mongodb

---
+++ client/src/app/crud-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { UserItem } from './UserItem';
+
+@Injectable({ providedIn: 'root' })
+export class CrudServiceService {
+  constructor(private http: HttpClient) { }
+
+
+  getAllUsers(): Observable<UserItem[]> {
+    return this.http.get<UserItem[]>('http://localhost:3000/users/api/');
+  }
+
+  insertUser(newUser: UserItem): Observable<UserItem> {
+    return this.http.post<UserItem>('http://localhost:3000/users/api/', newUser);
+  }
+
+  // since we don't need to wait for the delete to happen, not using observable
+  deleteUser(_id: Number) {
+    return this.http.delete('http://localhost:3000/users/api/' + _id);
+  }
+
+  updateUser(_id: Number, updatedUser: UserItem): Observable<void> {
+    return this.http.put<void>('http://localhost:3000/users/api/' + _id, updatedUser);
+  }
+
+}
+
