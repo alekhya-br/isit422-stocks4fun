e3745d71d658f3265cc3b11d285ded537d185f03
noah1984 <noahallen1984@hotmail.com>
2018-11-25 14:32:33 -0800

    placed quote search code in buystocks component

--- client/src/app/buystocks/buystocks.component.ts
+++ client/src/app/buystocks/buystocks.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MarketDataItem } from '../MarketDataItem';
+import { StockDataItem } from '../StockDataItem';
+import { StockService } from '../stock-service.service';
 
 @Component({
   selector: 'app-buystocks',
   templateUrl: './buystocks.component.html',
   styleUrls: ['./buystocks.component.scss']
 })
-export class BuystocksComponent implements OnInit {
 
-  constructor() { }
+export class BuystocksComponent {
+  @Input() stocks: StockDataItem;
 
-  ngOnInit() {
+  searchText: string = 'TSLA';
+  searchResult: StockDataItem[];
+  showChange: boolean;
+
+  getStock(term: string): void {
+    //const id = +this.route.snapshot.paramMap.get('id');
+    this.myStockService.searchQuotes(term).subscribe((searchResult: StockDataItem[]) => {
+      const showChange = searchResult.findIndex(w => w.change_pct !== undefined) >= 0;
+      this.searchResult = searchResult;
+      this.showChange = showChange;
+      console.log(`getStock result,`)
+      console.log(searchResult)
+    })
+    console.log('getStock called');
+    // .subscribe(stocks => this.stocks = stocks);
   }
+
+  constructor(
+    private myStockService: StockService) { }
 
 }
