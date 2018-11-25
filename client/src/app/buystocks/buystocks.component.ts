f2b0e60bf0d8ef6821fe1e92d718bde6fb9cb71c
noah1984 <noahallen1984@hotmail.com>
2018-11-25 14:22:35 -0800

    created buystocks component

---
+++ client/src/app/buystocks/buystocks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buystocks',
+  templateUrl: './buystocks.component.html',
+  styleUrls: ['./buystocks.component.scss']
+})
+export class BuystocksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
