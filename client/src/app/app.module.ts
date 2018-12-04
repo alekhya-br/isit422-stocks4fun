df0213b709ce5562b6a0aaea137f3ec85f1af402
noah1984 <noahallen1984@hotmail.com>
2018-12-03 20:56:27 -0800

    removed marketperformace component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,7 +15,6 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { QuotesearchComponent } from './quotesearch/quotesearch.component';
 import { BuystocksComponent } from './buystocks/buystocks.component';
-import { MarketperformanceComponent } from './marketperformance/marketperformance.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserComponent } from './user/user.component';
@@ -26,7 +25,6 @@ const appRoutes: Routes = [
   { path: 'home', component: HomepageComponent },
   { path: 'quotesearch', component: QuotesearchComponent},
   { path: 'buystocks', component: BuystocksComponent, resolve: { data: UserResolver }},
-  { path: 'marketperformance', component: MarketperformanceComponent},
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'register', component: SignUpComponent, canActivate: [AuthGuard] },
   { path: 'user', component: UserComponent, resolve: { data: UserResolver }},
