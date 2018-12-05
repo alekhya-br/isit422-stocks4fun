92b3f314eb5a8c8edbd5407abc72c985717ebb2b
noah1984 <noahallen1984@hotmail.com>
2018-12-04 16:15:41 -0800

    selling now functional

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -16,7 +16,7 @@ export class UserComponent implements OnInit {
 
   theOrders: OrderItem[];
 
-  stockID = 0;
+  orderID = 0;
   stockSymbol = 'unknown';
   stockName = 'unknown';
   stockPrice = 0.0;
@@ -26,8 +26,8 @@ export class UserComponent implements OnInit {
   user: FirebaseUserModel = new FirebaseUserModel();
   profileForm: FormGroup;
 
-  setModalFields(stockID, stockSymbol, stockName, stockQuantity, stockPrice) {
-    this.stockID = stockID;
+  setModalFields(orderID, stockSymbol, stockName, stockQuantity, stockPrice) {
+    this.orderID = orderID;
     this.stockSymbol = stockSymbol;
     this.stockName = stockName;
     this.stockQuantity = stockQuantity;
@@ -57,12 +57,42 @@ export class UserComponent implements OnInit {
   }
 
   sellStock(sellQuantity: number) {
-    this.stockID = 0;
-    this.stockName = 'unknown';
-    this.stockSymbol = 'unknown';
-    this.stockPrice = 0.0;
-    this.stockQuantity = 0;
-    alert("Successfully sold " + sellQuantity + " stocks.");
+    if(sellQuantity > 0) {
+      if(sellQuantity < this.stockQuantity) {
+        var updateItem: OrderItem = { _id: Object, user_uid: this.user.uid, stock_symbol: this.stockSymbol, stock_name: this.stockName, stock_price: this.stockPrice, stock_quantity: this.stockQuantity - sellQuantity };
+        this.myCrudService.updateOrder(this.orderID, updateItem)
+          .subscribe(
+            // nothing to do
+          );
+        //for (pointer; pointer >= 0; pointer--) {
+        //  if (this.theOrders[pointer]._id === this.orderID as Number) {
+        //    this.theOrders[pointer].stock_quantity = this.stockQuantity - sellQuantity;
+        //  }
+        //};
+        this.orderID = 0;
+        this.stockName = 'unknown';
+        this.stockSymbol = 'unknown';
+        this.stockPrice = 0.0;
+        this.stockQuantity = 0;
+        alert("Successfully sold " + sellQuantity + " stocks.");
+        window.location.reload();
+      }
+      else {
+        //delete db entry
+        this.myCrudService.deleteOrder(this.orderID) // it is a base 16 number
+          .subscribe();
+        // update local data
+        var pointer = this.theOrders.length - 1;
+        for (pointer; pointer >= 0; pointer--) {
+          if (this.theOrders[pointer]._id === this.orderID as Number) {
+            this.theOrders.splice(pointer, 1);
+          }
+        };
+      }
+    }
+    else {
+      alert("No stocks sold.");
+    }
   }
 
   getOrders(): void {
